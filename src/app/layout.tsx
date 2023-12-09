f08356f856f848e949008b46a5ac626c86835eaf
root <root@DESKTOP-NL0OPB4>
2023-12-09 15:49:13 +0300

    app layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
-import PageWrapper from "@/components/PageWrapper"
-import { bodyStyles } from "@/views/AppView/style.css"
-import React from "react"
+import PageWrapper from "@/components/PageWrapper";
+import React from "react";
 
 interface AppLayoutInterface {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }
 
 export default function AppLayout(props: AppLayoutInterface) {
-  return <html><body className={bodyStyles}><PageWrapper>{props.children}</PageWrapper></body></html>
+  return (
+    <html>
+      <body>
+        <PageWrapper>{props.children}</PageWrapper>
+      </body>
+    </html>
+  );
 }
